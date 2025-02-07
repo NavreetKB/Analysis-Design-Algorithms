1491b10f32c92eb18549ac096be14a2e314e9178
Navreet Kaur <166969655+NavreetKB@users.noreply.github.com>
2025-02-07 22:39:06 +0530

    Update Lab03-KnapSack.cpp

--- Lab03-KnapSack.cpp
+++ Lab03-KnapSack.cpp
@@ -5,7 +5,7 @@
 #include<iomanip>
 
 using namespace std;
-int partitionByWt(int arr[2][7], int s,int e){
+int partitionByWt(int arr[2][15], int s,int e){
         int pivot=arr[1][s];
         int i=s,j=e;
         while(i<j){
@@ -25,7 +25,7 @@ int partitionByWt(int arr[2][7], int s,int e){
         return j;
 }
 
-int partitionByProfit(int arr[2][7], int s,int e){
+int partitionByProfit(int arr[2][15], int s,int e){
         int pivot=arr[0][s];
         int i=s,j=e;
         while(i<j){
@@ -45,15 +45,7 @@ int partitionByProfit(int arr[2][7], int s,int e){
         return j;
 }
 
-void quicksort(int arr[2][7],int s,int e){
-        if(s>=e) return;
-        int p=partitionByWt(arr,s,e);
-        quicksort(arr,s,p-1);
-        
-        quicksort(arr,p+1,e);
-}
-
-int partitionPbyW(float arr[3][7], int s,int e){
+int partitionPbyW(float arr[3][15], int s,int e){
         int pivot=arr[2][s];
         int i=s,j=e;
         while(i<j){
@@ -75,7 +67,23 @@ int partitionPbyW(float arr[3][7], int s,int e){
         return j;
 }
 
-void quicksort2(float arr[3][7],int s,int e){
+
+void quicksort(int arr[2][15],int s,int e,bool flag){
+        if(s>=e) return;
+        int p;
+        if(flag==true){
+            p=partitionByProfit(arr,s,e);
+        }
+        else{
+            p=partitionByWt(arr,s,e);
+        }
+        quicksort(arr,s,p-1,flag);
+        
+        quicksort(arr,p+1,e,flag);
+}
+
+
+void quicksort2(float arr[3][15],int s,int e){
         if(s>=e) return;
         int p=partitionPbyW(arr,s,e);
         quicksort2(arr,s,p-1);
@@ -84,10 +92,10 @@ void quicksort2(float arr[3][7],int s,int e){
 }
 
 
-float* KnapSack(int arr[2][7],float c){
-    float *result=new float[7]{0};
-    for(int i=0;i<7;i++){
-        if(c<=0){
+float* KnapSack(int arr[2][15],float c){
+    float *result=new float[15]{0.0};
+    for(int i=0;i<15;i++){
+        if(c<=0  || std::abs(c) < 1e-6){
             result[i]=0;
         }
         else if(arr[1][i]<=c){
@@ -95,7 +103,6 @@ float* KnapSack(int arr[2][7],float c){
            c=c-arr[1][i];
         }
         else if(c!=0){
-        
           float f=c/(arr[1][i]);
           result[i]=f;
           float fractionWt=(arr[1][i])*f;
@@ -106,9 +113,11 @@ float* KnapSack(int arr[2][7],float c){
     return result;
 }
 
-float* knapWeight(int arr[2][7],float c){
-        float *result=new float[7]{0};
-    for(int i=0;i<7;i++){
+
+
+float* knapPW(float arr[3][15],float c){
+        float *result=new float[15]{0};
+    for(int i=0;i<15;i++){
         if(c<=0){
             result[i]=0;
         }
@@ -128,107 +137,148 @@ float* knapWeight(int arr[2][7],float c){
     return result;
 }
 
-float* knapPW(float arr[3][7],float c){
-        float *result=new float[7]{0};
-    for(int i=0;i<7;i++){
-        if(c<=0){
-            result[i]=0;
-        }
-        else if(arr[1][i]<=c){
-           result[i]=1;
-           c=c-arr[1][i];
-        }
-        else if(c!=0){
-        
-          float f=c/(arr[1][i]);
-          result[i]=f;
-          float fractionWt=(arr[1][i])*f;
-          c=c-fractionWt;
+void initialise(int arr[2][15]){
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<15;j++){
+            arr[i][j]=(rand()%100+1);
         }
     }
-       
-    return result;
 }
 
 int main(){
-    int arr[2][7]={{12,14,10,7,5,15,9},
-                   {2,35,7,15,5,7,3}};
+    // int arr[2][15]={{12,14,10,7,5,15,9,22,11,2,4,16,17,21,18},
+    //                {2,35,7,15,5,7,3,10,11,12,7,3,6,8,22}};
+    int arr[2][15]={0};
     
+    initialise(arr);
+    int C=100;
     int temp[2][7]={0};
     for(int i=0;i<2;i++){
         for(int j=0;j<7;j++){
             temp[i][j]=arr[i][j];
         }
+        
     }
 
-    quicksort(arr,0,6);
-    float C=25;
-    cout<<"sorted in non increasing order"<<endl;
-    for(int i=0;i<7;i++){
-        cout<<arr[0][i]<<"  ";
-    }
-    cout<<endl;
-    for(int i=0;i<7;i++){
-        cout<<arr[1][i]<<"  ";
-    }
-    cout<<endl;
-    
-    float* output=knapWeight(arr,C);
-    float answer[7]={0};
-    cout<<endl<<endl;
-     for(int i=0;i<2;i++){
-          for(int j=0;j<7;j++){
-           cout<<temp[i][j]<<"  ";
-          }
-          cout<<endl;
-        }  
-    cout<<"answer : "<<endl;
-
-        for(int i=0;i<7;i++){
-          for(int j=0;j<7;j++){
-            if(arr[0][i]==temp[0][j])
-            {
-                answer[j]=output[i];
-            }
-          }
+    cout<<setw(80)<<"**************** Sorted by profit ******************\n";
+    bool flag=true;
+    quicksort(arr,0,14,flag);
+    for(int p=0;p<16;p++){
+            cout<<"-------+";
         }
-    for(int i=0;i<7;i++){
-        cout<<answer[i]<<"  ";
-    }
-    
-
-    cout<<"\n******Using Profit by weight******\n";
-    float arr2[3][7]={0};
+        cout<<endl;
     for(int i=0;i<2;i++){
-          for(int j=0;j<7;j++){
-            arr2[i][j]=temp[i][j];
-          }
-        }    
-    for(int i=0;i<7;i++){
-        arr2[2][i]=float(temp[0][i])/float(temp[1][i]);
-    }
-
-    for(int i=0;i<3;i++){
-        for(int j=0;j<7;j++){
-            cout<<setw(5)<<arr2[i][j]<<"  ";
+        
+        if(i==0)
+           cout<<"PROFIT |";
+        if(i==1)
+           cout<<"WEIGHT |";
+        for(int j=0;j<15;j++){
+            cout<<setw(4)<<arr[i][j]<<setw(4)<<"|";
+        }
+        cout<<endl;
+        for(int k=0;k<16;k++){
+            cout<<"-------+";
         }
         cout<<endl;
     }
 
-    quicksort2(arr2,0,6);
-    cout<<"sorted profit by weight"<<endl;
-    for(int i=0;i<3;i++){
-        for(int j=0;j<7;j++){
-            cout<<setw(5)<<arr2[i][j]<<"  ";
-        }
-        cout<<endl;
-    }
-    float* answer3=knapPW(arr2,C);
-        for(int i=0;i<7;i++){
-        cout<<answer3[i]<<"  ";
-    }
+    float* answer=KnapSack(arr,C);
 
-
+    cout<<"RESULT |";
+    for(int i=0;i<15;i++){
+       cout<<setw(4)<<setprecision(2)<<answer[i]<<setw(4)<<"|";
+    }
+    cout<<endl;
+    for(int p=0;p<16;p++){
+        cout<<"-------+";
+    }
+    cout<<endl<<endl;
     
+    cout<<setw(80)<<"**************** Sorted by Weight ******************\n";
+    flag=false;
+    quicksort(arr,0,14,flag);
+    for(int p=0;p<16;p++){
+            cout<<"-------+";
+        }
+        cout<<endl;
+    for(int i=0;i<2;i++){
+        
+        if(i==0)
+           cout<<"PROFIT |";
+        if(i==1)
+           cout<<"WEIGHT |";
+        for(int j=0;j<15;j++){
+            cout<<setw(4)<<arr[i][j]<<setw(4)<<"|";
+        }
+        cout<<endl;
+        for(int k=0;k<16;k++){
+            cout<<"-------+";
+        }
+        cout<<endl;
+    }
+
+    float* ansWt=KnapSack(arr,C);
+
+    cout<<"RESULT |";
+    for(int i=0;i<15;i++){
+       cout<<setw(4)<<setprecision(2) <<ansWt[i]<<setw(4)<<"|";
+    }
+    cout<<endl;
+    for(int p=0;p<16;p++){
+        cout<<"-------+";
+    }
+
+    cout<<endl;
+    
+    cout<<setw(100)<<"**************** Sorted by Profit per unit Weight ***************\n";
+    float arr2[3][15]={0};
+    for(int i=0;i<2;i++){
+        for(int j=0;j<15;j++){
+            arr2[i][j]=temp[i][j];
+        }
+    }
+    for(int i=0;i<15;i++){
+        arr2[2][i] = float(temp[0][i])/float(temp[1][i]);
+       
+    } 
+    quicksort2(arr2,0,14);
+    for(int p=0;p<16;p++){
+            cout<<"-------+";
+        }
+        cout<<endl;
+    for(int i=0;i<3;i++){
+        
+        if(i==0)
+           cout<<"PROFIT |";
+        if(i==1)
+           cout<<"WEIGHT |";
+        if(i==2)
+           cout<<"P/W    |";
+        for(int j=0;j<15;j++){
+            cout<<setw(4)<<setprecision(2)<<arr2[i][j]<<setw(4)<<"|";
+        }
+        cout<<endl;
+        for(int k=0;k<16;k++){
+            cout<<"-------+";
+        }
+        cout<<endl;
+    }
+
+    float* ansPW=knapPW(arr2,C);
+
+    cout<<"RESULT |";
+    for(int i=0;i<15;i++){
+       cout<<setw(4)<<setprecision(2)<<ansPW[i]<<setw(4)<<"|";
+    }
+    cout<<endl;
+    for(int p=0;p<16;p++){
+        cout<<"-------+";
+    }
+
+    cout<<endl;
+    
+    return 0;
 
 }
